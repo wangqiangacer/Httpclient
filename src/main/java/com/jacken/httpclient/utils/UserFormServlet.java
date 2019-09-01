cc283f36aa6a32d46fd5eb29e5bca2779e5a52e4
wangqiang <850994281@qq.com>
2019-09-01 10:38:31 +0800

    HttpClient的使用发送get以及post请求

---
+++ src/main/java/com/jacken/httpclient/utils/UserFormServlet.java
@@ -0,0 +1,23 @@
+package com.jacken.httpclient.utils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/UserFormServlet")
+public class UserFormServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("userName");
+        //自定义请求头
+        String token = req.getHeader("token");
+        resp.getWriter().write("success");
+    }
+}
